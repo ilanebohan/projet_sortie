2e1daa5d29f239adb39e5aae2dcb71818f7baf4f
Raphaldeouf <raphaelvannier2001@gmail.com>
2023-11-09 09:33:01 +0100

    [RVR] Secure endpoints

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -17,7 +17,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[Route('/sortie')]
 class SortieController extends AbstractController
 {
 
