5f40d93e31cf56689391f1fe41f466a3dd6d8ef0
Ilane BOHAN <ilane.bohan2022@campus-eni.fr>
2023-11-10 10:42:12 +0100

    [IBO]Remember Me function

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,13 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
 
 class SecurityController extends AbstractController
 {
+    // random secret key
+    //private $secretKey = '';
 
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
@@ -20,9 +26,27 @@ class SecurityController extends AbstractController
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
+        if (isset($_COOKIE['remember_me']))
+        {
 
+            $lastUsername = openssl_decrypt($_COOKIE['remember_me'], "AES-128-ECB", $this->getParameter('secret_key'));
+        }
+        else
+        {
+            $lastUsername = $authenticationUtils->getLastUsername();
+        }
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error,'mailError' => '',]);
+    }
+
+    #[Route(path: '/disconnect', name: 'app_disconnect')]
+    public function disconnect(Request $request, Security $security)
+    {
+        if (isset($_COOKIE['REMEMBERME']))
+        {
+            $hash = openssl_encrypt($this->getUser()->getUserIdentifier(), "AES-128-ECB", $this->getParameter('secret_key'));
+            setcookie('remember_me',$hash, time() + 3600, '/');
+        }
+            return $this->redirectToRoute('app_logout');
     }
 
 
